c9d6012ec4b6ebd86bf55e52272cf7ed3a10edb9
Nitin Kumar <nk095291@gmail.com>
2024-02-23 01:05:11 +0530

    pr-1 : Added code to upload image file instead of adding just image path

--- projects/models.py
+++ projects/models.py
@@ -6,4 +6,4 @@ class Project(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
     technology = models.CharField(max_length=20)
-    image = models.CharField(max_length=100)
+    image = models.ImageField(upload_to="projects/img") 
